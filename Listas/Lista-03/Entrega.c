c13df9fd23362691eea3a3deb3f41434b4f473fb
Wendi Ramos <wendiramos12@gmail.com>
2022-10-06 23:11:30 -0300

    corrigindo Lista-03 Ex-05

--- Listas/Lista-03/Entrega.c
+++ Listas/Lista-03/Entrega.c
@@ -1,6 +1,105 @@
 #include <stdio.h>
 #include <math.h>
 
+typedef struct sCarros
+    {
+        char placa[15];
+        char cor[15];
+        int ano;
+    } Carro;
+
+
+void cadastrar(Carro *pCar, int contCar)
+{
+    printf("------------ Cadastrar Carro ----------\n");
+    printf("Entre com as informacoes do novo carro:\n");
+
+    printf("Placa: ");
+    scanf("%s", pCar[contCar].placa);
+
+    printf("Cor: ");
+    scanf("%s", pCar[contCar].cor);
+
+    printf("Ano: ");
+    scanf("%d", &pCar[contCar].ano);
+}
+
+void editar(Carro *pCar, int contCar)
+{ 
+    int i;
+    char placa[15], posicao = -1;
+    printf("======================= EDITAR ======================\n");
+    printf("Entre com a placa do veiculo a ser editado: ");
+    scanf(" %[^\n]", placa);
+    for ( i = 0; i < contCar; i++)
+    {
+        if (strcmp(pCar[i].placa, placa) == 0)
+        {
+            posicao = i;
+            break;
+        }
+    }
+    if (posicao == -1)
+    {
+        printf("Nao encontrado :(\n");
+    }
+    else
+    {
+        printf("===================== VEICULO %d =====================\n", posicao + 1);
+        printf("| PLACA: %s\n", pCar[posicao].placa);
+        printf("| COR:   %s\n", pCar[posicao].cor);
+        printf("| ANO:   %d\n", pCar[posicao].ano);
+        printf("======================================================\n\n");
+
+        int opcaoEditar = 0;
+
+        printf("======================== MENU ========================\n");
+        printf("| 1 ........................................... PLACA |\n");
+        printf("| 2 ............................................. COR |\n");
+        printf("| 3 ............................................. ANO |\n");
+        printf("| Outro .................................... CANCELAR |\n");
+        printf("======================================================\n");
+        scanf("%d", &opcaoEditar);
+
+        switch (opcaoEditar)
+        {
+        case 1:
+            printf("PLACA: ");
+            scanf("%s", pCar[posicao].placa);
+            break;
+        case 2:
+            printf("COR: ");
+            scanf("%s", pCar[posicao].cor);
+            break;
+        case 3:
+            printf("ANO: ");
+            scanf("%d", &pCar[posicao].ano);
+            break;
+        }
+    }
+}
+
+void listar(Carro *pCar, int contCar)
+{ 
+    int i;
+    if (contCar == 0)
+    {
+        printf("Nenhum carro cadastrado :(\n");
+    }
+    else
+    {
+        printf("======================= LISTAR ======================\n");
+        for ( i = 0; i < contCar; i++)
+        {
+            printf("===================== VEICULO %d =====================\n", i + 1);
+            printf("| PLACA: %s\n", pCar[i].placa);
+            printf("| COR:   %s\n", pCar[i].cor);
+            printf("| ANO:   %d\n", pCar[i].ano);
+            printf("======================================================\n\n");
+        }
+    }
+}
+
 void Ex01()
 {
     int *x, *y;
@@ -54,14 +153,14 @@ void Ex02()
 
 void Ex03()
 {
-  typedef struct cli
-{
-    char nome[20];
-    char telefone[12];
-    int idade;
-} CLIENTE;
+    typedef struct cli
+    {
+        char nome[20];
+        char telefone[12];
+        int idade;
+    } CLIENTE;
 
-int i, num;
+    int i, num;
     CLIENTE *c;
 
     printf("Quantos clientes deseja cadastrar? \n");
@@ -112,7 +211,7 @@ void Ex04()
     {
         matriz[x] = (int *)malloc(colunas * sizeof(int));
     }
-    
+
     for (x = 0; x < linhas; x++)
     {
         for (y = 0; y < colunas; y++)
@@ -120,7 +219,7 @@ void Ex04()
             matriz[x][y] = (rand() % 100) + 1;
         }
     }
-    
+
     for (x = 0; x < linhas; x++)
     {
         for (y = 0; y < colunas; y++)
@@ -129,7 +228,7 @@ void Ex04()
         }
         printf("\n");
     }
-    
+
     for (x = 0; x < linhas; x++)
     {
         free(matriz[x]);
@@ -141,46 +240,35 @@ void Ex04()
 
 void Ex05()
 {
-     int **matriz, x, y, z;
-    int linhas, colunas;
+    int n = 0, opcao;
+    Carro carros[100];
 
-    printf("Entre com o numero de linhas: \n");
-    scanf("%d", &linhas);
-    printf("Entre com o numero de colunas: \n");
-    scanf("%d", &colunas);
+    do
+    {
+        printf("Digite a opcao desejada: \n");
+        printf("1 - Cadastrar\n");
+        printf("2 - Editar\n");
+        printf("3 - Listar\n");
+        printf("0 - Sair\n");
+        scanf("%d", &opcao);
 
-    matriz = (int **)malloc(linhas * sizeof(int *));
-    for (x = 0; x < linhas; x++)
-    {
-        matriz[x] = (int *)malloc(colunas * sizeof(int));
-    }
-    
-    for (x = 0; x < linhas; x++)
-    {
-        for (y = 0; y < colunas; y++)
+        switch (opcao)
         {
-            matriz[x][y] = (rand() % 100) + 1;
+        case 1:
+            cadastrar(&carros, n);
+            n++;
+            break;
+        case 2:
+            editar(&carros, n);
+            break;
+        case 3:
+            listar(&carros, n);
+            break;
         }
-    }
-    
-    for (x = 0; x < linhas; x++)
-    {
-        for (y = 0; y < colunas; y++)
-        {
-            printf("%d\t", matriz[x][y]);
-        }
-        printf("\n");
-    }
-    
-    for (x = 0; x < linhas; x++)
-    {
-        free(matriz[x]);
-    }
-    free(matriz);
-    matriz = NULL;
+    } while (opcao != 0);
+
     return 0;
 }
-
 
 int main()
 {
@@ -227,7 +315,7 @@ int main()
             Ex05();
             break;
         }
-       
+
         default:
         {
             if (opcao == 0)
